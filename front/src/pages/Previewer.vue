d7f44bbc47076bd7e1d729657a5d84964d8a10e7
Administrator <1971701189a@gmail.com>
2024-03-27 17:28:03 +0800

    preview pdf

---
+++ front/src/pages/Previewer.vue
@@ -0,0 +1,30 @@
+<script setup>
+import VuePdfEmbed from 'vue-pdf-embed'
+import route from "@/router.js";
+// essential styles
+import 'vue-pdf-embed/dist/style/index.css'
+import 'vue-pdf-embed/dist/style/annotationLayer.css'
+import 'vue-pdf-embed/dist/style/textLayer.css'
+import {useRoute} from "vue-router";
+import {onMounted, ref} from "vue";
+
+const router = useRoute()
+let source = ref(0)
+onMounted(() => {
+  source.value = router.params.pdfUrl
+  source.value = "http://127.0.0.1:8000/preview?" + source.value
+})
+// onMounted(() => {
+//   // getPagesNumber()
+// })
+</script>
+<template>
+  {{source}}
+  <div class="pdf-wrapper">
+    <vue-pdf-embed ref="pdfRef" :source="source"/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- front/src/router.js
+++ front/src/router.js
@@ -1,18 +1,24 @@
 import Home from "@/pages/Home.vue";
 import {createRouter, createWebHistory} from "vue-router";
 import tutorial from "@/pages/Tutorial.vue";
+import Previewer from "@/pages/Previewer.vue";
 
 const routes = [
     {
-            path: "/",
-            name: "Home",
-            component: Home
-        },
-        {
-            path: "/tutorial",
-            name: "tutorial",
-            component: tutorial
-        }
+        path: "/",
+        name: "Home",
+        component: Home
+    },
+    {
+        path: "/tutorial",
+        name: "tutorial",
+        component: tutorial
+    },
+    {
+        path: "/preview/:pdfUrl",
+        name: "preview",
+        component: Previewer
+    }
 ]
 
 const router = createRouter({
