cc93b4ac116c36b4906e39e8172ea7941908c60d
233kun <1971701189a@gmail.com>
2024-12-01 01:40:34 +0800

    refactor: front config

---
+++ front/config.js
@@ -0,0 +1,7 @@
+window.config = {
+    baseURL: 'https://print-backend.233kun.top:8000',
+    title: '30',
+    price: 0.2,
+    discountedPrice: 0.15
+}
+
--- front/dist/static/config.js
+++
@@ -1,7 +0,0 @@
-window.config = {
-    baseURL: '',
-    title: '',
-    price: 0.2,
-    discountedPrice: 0.15
-}
-
