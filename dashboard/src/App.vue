5e6e25c7636dd6bd648b69a4faadc912a49d77c0
233kun <1971701189a@gmail.com>
2025-03-04 14:16:56 +0800

    Init: dashboard

---
+++ dashboard/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import Dashboard from "@/dashboard.vue";
+</script>
+
+<template>
+  <dashboard></dashboard>
+</template>
+
+<style scoped>
+
+</style>
