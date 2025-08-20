75f55b83a9112cad572c2329937a892d49bce108
233kun <1971701189a@gmail.com>
2025-08-21 07:16:33 +0800

    fix docs convert of convert_wps.py

--- convert_wps.py
+++ convert_wps.py
@@ -1,3 +1,7 @@
+import logging
+import traceback
+
+from pypdf import PdfReader
 from pywpsrpc.rpcetapi import createEtRpcInstance
 from pywpsrpc.rpcwpsapi import (createWpsRpcInstance, wpsapi)
 from pywpsrpc import RpcIter
@@ -18,20 +22,39 @@ class ConvertWPS(ConvertUtil):
             if file_attributes.filename == filename:
                 file_attributes.convert_state = 'processing'
         files_attributes_global.data.update({directory: files_attributes})
-        hr, doc = app.Documents.Open(f'uploads/{directory}/raw/{filename}')
-        def onDocumentBeforeSave(doc):
-            print("onDocumentBeforeSave called for doc: ", doc.Name)
-            return True, False
+        try:
+            hr, doc = app.Documents.Open(f'uploads/{directory}/raw/{filename}')
+            def onDocumentBeforeSave(doc):
+                print("onDocumentBeforeSave called for doc: ", doc.Name)
+                return True, False
 
-        rpc.registerEvent(app,
-                          wpsapi.DIID_ApplicationEvents4,
-                          "DocumentBeforeSave",
-                          onDocumentBeforeSave)
+            rpc.registerEvent(app,
+                              wpsapi.DIID_ApplicationEvents4,
+                              "DocumentBeforeSave",
+                              onDocumentBeforeSave)
 
-        output_filename = filename.rsplit(".", 1)[0] + '.pdf'
-        doc.SaveAs2(f'uploads/{directory}/converted/{output_filename}', 17)
+            output_filename = filename.rsplit(".", 1)[0] + '.pdf'
+            doc.SaveAs2(f'uploads/{directory}/converted/{output_filename}', 17)
 
-        app.Quit(wpsapi.wdDoNotSaveChanges)
+            for file_attributes in files_attributes:
+                if file_attributes.filename == filename:
+                    file_attributes.convert_state = 'success'
+                    converted_filename = file_attributes.filename.rsplit(".", 1)[0] + '.pdf'
+                    reader = PdfReader(f"uploads/{directory}/converted/{converted_filename}")
+                    file_attributes.total_pages = len(reader.pages)
+                    file_attributes.print_range_end = len(reader.pages)
+
+            files_attributes_global.data.update({directory: files_attributes})
+        except Exception as e:
+            for file_attributes in files_attributes:
+                if file_attributes.filename == filename:
+                    file_attributes.convert_state = 'error'
+            files_attributes_global.data.update({directory: files_attributes})
+            print(e)
+            logging.error(f'Exception while converting DOC\nFilename: {filename}', e)
+        finally:
+            app.Quit(wpsapi.wdDoNotSaveChanges)
+
     @staticmethod
     def convert_excel(directory, filename):
         hr, rpc = createEtRpcInstance()
