244caeefb131b13ee0df6ba4a662217d18ff04a9
233kun <1971701189a@gmail.com>
2025-09-22 15:41:50 +0800

    refactor printer client

--- client/main.py
+++ client/main.py
@@ -1,66 +1,88 @@
-import http.client
+import os
 import json
 import logging
-import os.path
 import subprocess
 import urllib.request
-from time import sleep, time
+from time import sleep
 from urllib.parse import quote
-SERVER_HOST = '47.106.100.54:8000'
-PRINTER_URL = 'http://192.168.123.138:631/printers/HP_LaserJet_P2015_Series'
-SECRET_KEY = '123456789'
-PRINTING_TIMEOUT_PRE_PAGE = 10
-log_format = '%(asctime)s - %(levelname)s - %(message)s'
-logging.basicConfig(level=logging.INFO, format=log_format)
-logger = logging.getLogger()
 
-def polling_jobs(interval):
-    while True:
-        sleep(interval)
+import setting
 
-        connection = http.client.HTTPSConnection(SERVER_HOST, timeout=5)
-        connection.request('GET', '/printer/jobs')
-        response_body = connection.getresponse().read().decode("UTF-8")
-        connection.close()
+logging.basicConfig(
+    level=logging.INFO,  # 设置日志级别为INFO，会记录INFO及以上级别
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
-        response_dict = json.loads(response_body)
-        if response_dict.get('data').get('jobs') is None:
-            continue
-        logging.info(f'Received response from server: Response = {response_body}')
+def get_new_job():
+    try:
+        req = urllib.request.Request(f"{setting.SERVER_HOST}/printer/jobs")
+        req.add_header("Content-Type", "application/json")
+        req.add_header("Accept", "application/json")
+        req.add_header("Authorization", f"Bearer {setting.SECRET_KEY}")
+        res = urllib.request.urlopen(req)
 
-        job_attrs = response_dict.get('data').get('jobs')
-        path = job_attrs.get('folder')
-        filename = job_attrs.get('filename')
-        filename_pdf = filename.rsplit('.', 1)[0] + '.pdf'
-        try:
-            response = urllib.request.urlopen(f"https://{SERVER_HOST}/printer/file?path={path}&filename={quote(filename_pdf)}", timeout=5)
-        except Exception as e:
-            logger.error(e)
-        else:
-            json_body = {'filename': job_attrs.get('filename'),
-                         'folder': job_attrs.get('folder'),
-                         'state': 'success',
-                         'authentication': SECRET_KEY}
-            params = json.dumps(json_body)
-            headers = {"Content-type": "application/json",
-                       "accept": "application/json"}
-            connection = http.client.HTTPSConnection(SERVER_HOST, timeout=5)
-            connection.request('POST', '/printer/job/state', params, headers)
-        if not os.path.exists(f'downloads/{path}/'):
-            os.mkdir(f'downloads/{path}')
-        with open(f"downloads/{path}/{filename_pdf}", 'wb') as f:
-            f.write(response.read())
-        return job_attrs
+        job_attributes = json.loads(res.read().decode("utf-8")).get("data").get("jobs")
+        if job_attributes is None:
+            return None
+        return job_attributes
+    except urllib.request.HTTPError as e:
+        logging.error("Network Error: Unable to connect to server")
+        logging.error(e)
+    except json.decoder.JSONDecodeError as e:
+        logging.error("JSON Decode Error: Failed to decode JSON")
+        logging.error(e)
+    except Exception as e:
+        logging.error("Unexpected error occurred")
+        logging.error(e)
 
 
-def execute_ipptool(job_attributes):
-    print_copies = job_attributes.get('print_copies')
-    print_range_start = job_attributes.get('print_range_start')
-    print_range_end = job_attributes.get('print_range_end')
-    print_ranges = str(print_range_start) + '-' + str(print_range_end)
-    print_file_path = job_attributes.get('folder')
-    print_filename = job_attributes.get('filename').rsplit('.', 1)[0] + '.pdf'
-    print_side = job_attributes.get('print_side')
+def download_job_file(job_attributes):
+    folder = job_attributes.get("folder")
+    filename = job_attributes.get("filename")
+    try:
+        req = urllib.request.Request(
+            f"{setting.SERVER_HOST}/printer/file?path={folder}&filename={quote(filename)}"
+        )
+        req.add_header("Content-Type", "application/json")
+        req.add_header("Accept", "application/json")
+        req.add_header("Authorization", f"Bearer {setting.SECRET_KEY}")
+        res = urllib.request.urlopen(req)
+
+        if not os.path.exists(f"downloads/{folder}"):
+            os.mkdir(f"downloads/{folder}")
+        with open(f"downloads/{folder}/{filename}", "wb") as f:
+            f.write(res.read())
+        params_body = {
+            "filename": filename,
+            "folder": folder,
+            "state": "success",
+            "authentication": setting.SECRET_KEY,
+        }
+        params = json.dumps(params_body).encode("utf-8")
+        req = urllib.request.Request(f"{setting.SERVER_HOST}/printer/job/state", params)
+        req.add_header("Content-Type", "application/json")
+        req.add_header("Accept", "application/json")
+        req.add_header("Authorization", f"Bearer {setting.SECRET_KEY}")
+        res = urllib.request.urlopen(req)
+        return f"{folder}/{filename}"
+    except urllib.request.HTTPError as e:
+        logging.error("Network Error: Unable to connect to server")
+        logging.error(e)
+    except Exception as e:
+        logging.error("Unexpected error occurred")
+        logging.error(e)
+
+
+def exec_printing_linux(job_attributes):
+    print_copies = job_attributes.get("print_copies")
+    print_range_start = job_attributes.get("print_range_start")
+    print_range_end = job_attributes.get("print_range_end")
+    print_range = str(print_range_start) + "-" + str(print_range_end)
+    print_side = job_attributes.get("print_side")
+    folder = job_attributes.get("folder")
+    filename = job_attributes.get("filename")
+
     if print_side == 'one-sided':
         print_side = 'one-sided'
     elif print_side == 'two-sided-default':
@@ -69,64 +91,47 @@ def execute_ipptool(job_attributes):
         print_side = 'two-sided-long-edge'
     elif print_side == 'two-sided-short-edge':
         print_side = 'two-sided-short-edge'
-    ipp_file_path = os.path.abspath("ipp_files/print_ticket_attributes.ipp")
-    submit_print_job = subprocess.run(
-        f"ipptool -tv {PRINTER_URL} -f "
-        f"'downloads/{print_file_path}/{print_filename}' -d sides={print_side} -d page-ranges={print_ranges} -d "
-        f"copies={print_copies} {ipp_file_path}",
+    try:
+        exec_print = subprocess.run(
+            [
+                "ipptool", "-tv", setting.PRINTER_URL, "-f", f"downloads/{folder}/{filename}", "-d", f"sides={print_side}", "-d", f"page-ranges={print_range}", "-d", f"copies={print_copies}", "ipp_files/print_ticket_attributes.ipp"
+            ],
         capture_output=True,
-        shell=True,
-        text=True)
-    if submit_print_job.stdout == '':
-        raise '[ERROR] Fail to execute job'
-    job_id = submit_print_job.stdout.split("\n")[14][27:]
-    return job_id
+        text=True
+        )
+        if exec_print.returncode == 0:
+            if exec_print.stdout.find("job-id") == -1:
+                logging.error("Print Error: Failed to print job, job_attributes:")
+                logging.error(job_attributes)
+                logging.error("stout:")
+                logging.error(exec_print.stdout)
+            else:
+                if os.path.exists(f"downloads/{folder}/{filename}"):
+                    os.remove(f"downloads/{folder}/{filename}")
+                    if len(os.listdir(f"downloads/{folder}")) == 0:
+                        os.remove(f"downloads/{folder}")
+                logging.info("Success print, job_attributes:")
+                print(job_attributes)
+        else:
+            logging.error("Print Error: Failed to print job, job_attributes:")
+            print(job_attributes)
+            logging.error("stderr:")
+            print(exec_print.stderr)
+    except Exception as e:
+        print(exec_print.args)
+        logging.error("Unexpected error occurred")
+        logging.error(e)
+        return None
 
-
-def poll_cups_job_state(job_id, job_attributes, timeout, interval=5):
-    start_time = time()
+if __name__ == "__main__":
     while True:
-        ipp_file_path = os.path.abspath("ipp_files/job_attributes.ipp")
-        job_state = subprocess.run(
-            f"ipptool -tv {PRINTER_URL} -d job-id={job_id} {ipp_file_path}",
-            capture_output=True,
-            shell=True,
-            text=True)
-        if job_state.stdout == '':
-            raise '[ERROR] Fail to get job state'
-        start = job_state.stdout.find("job-state (enum) = ")
-        end = job_state.stdout.find("\n", start)
-        job_state = job_state.stdout[start + 19:end]
-        if job_state == "completed":
-            print_filename = job_attributes.get('filename').rsplit('.', 1)[0] + '.pdf'
-            if os.path.exists(f'downloads/{print_filename}'):
-                os.remove(f'downloads/{print_filename}')
-            return 'success'
-        if time() - start_time > timeout:
-            return 'error'
-        sleep(interval)
-
-
-if __name__ == '__main__':
-    while True:
-        try:
-            job_attributes = polling_jobs(interval=1)
-            job_id = execute_ipptool(job_attributes)
-            job_state = poll_cups_job_state(job_id, job_attributes, timeout=job_attributes.get('total_pages') * 10)
-        except Exception as e:
-            logger.error(e)
+        sleep(1)
+        job_attributes = get_new_job()
+        if job_attributes is None:
             continue
 
-        try:
-            if job_state == 'error':
-                json_body = {'filename': job_attributes.get('filename'),
-                             'folder': job_attributes.get('folder')}
-                params = json.dumps(json_body)
-                headers = {"Content-type": "application/json",
-                           "accept": "application/json"}
-                connection = http.client.HTTPSConnection("SERVER_HOST", timeout=5)
-                # connection.request('POST', '/printer/job/state')
-                # 等未来做好商家端的时候添加异常文件上报
-        except Exception as e:
-            logger.error(e)
+        file_path = download_job_file(job_attributes)
+        if file_path is None:
+            continue
 
+        exec = exec_printing_linux(job_attributes)
